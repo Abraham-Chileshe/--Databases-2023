8ef97536817a43536c19f3af9aa29f408cf9a62b
Chileshe <64306558+Abraham-Chileshe@users.noreply.github.com>
2023-10-11 14:22:28 +1000

    Create 02I.sql

---
+++ 02. Условия, UPDATE, DELETE/02I.sql
@@ -0,0 +1,15 @@
+/*
+В таблице Animals нужно изменить все значения поля Sex с длинных вариантов: "male", "female" на короткие: "m", "w". Для прочих значений поля Sex, нужно указать "unknown", если они не "NULL".
+Решение следует представить в виде текстового файла, содержащего несколько SQL-запросов.
+*/
+
+UPDATE Animals
+SET Sex = CASE
+    WHEN Sex = 'male' THEN 'm'
+    WHEN Sex = 'female' THEN 'w'
+    ELSE Sex
+END;
+
+UPDATE Animals
+SET Sex = 'unknown'
+WHERE Sex IS NOT NULL;
