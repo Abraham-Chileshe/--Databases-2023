d93d1dff42c0e3075186c58cb701d0f770c18e28
Chileshe <64306558+Abraham-Chileshe@users.noreply.github.com>
2023-10-25 14:12:01 +1000

    Update 02I.sql

--- 02. Условия, UPDATE, DELETE/02I.sql
+++ 02. Условия, UPDATE, DELETE/02I.sql
@@ -13,3 +13,15 @@ END;
 UPDATE Animals
 SET Sex = 'unknown'
 WHERE Sex IS NOT NULL;
+
+/*
+Using Only One Script
+*/
+
+UPDATE Animals
+SET Sex = CASE
+    WHEN Sex = 'male' THEN 'm'
+    WHEN Sex = 'female' THEN 'w'
+    ELSE 'unknown'
+END
+WHERE Sex IS NOT NULL;
